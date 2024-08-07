aaa20f0a907b0b33d7e89b547b3871693bac7c4b
VoltageLord <Voltagelord0@gmail.com>
2024-08-07 23:21:16 +0100

    Voltage⚡

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const {
 const path = require('path')
 
 function start() {
-   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   let args = [path.join(__dirname, 'main.js'), ..process.argv.slice(2)]
    console.log([process.argv[0], ...args].join('\n'))
    let p = spawn(process.argv[0], args, {
          stdio: ['inherit', 'inherit', 'inherit', 'ipc']
